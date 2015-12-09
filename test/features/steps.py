2b84e0894cf73532ffdcaa86d0bb1a22865cabd2
David Ohlemacher <david.ohlemacher@verizon.com>
2015-12-09 00:39:48 -0500

    Add a jupyter notebook for docs.

--- test/features/steps.py
+++ test/features/steps.py
@@ -1,6 +1,5 @@
 
 '''
-Ref: https://semaphoreci.com/community/tutorials/bdd-testing-a-restful-web-application-in-python
 Allow defining steps and store values to be used across each step in the
 world object.
 '''
@@ -8,14 +7,15 @@ from lettuce import step, world, before
 from nose.tools import assert_equals
 from app.application import app, USERS
 import json
-#from app.views import USERS
 
 @before.all
 def before_all():
+    '''Setup for testing.'''
     world.app = app.test_client()
 
 @step(u'Given some users are in the system')
 def given_some_users_are_in_the_system(step):
+    '''Add a user to USERS for testing against.'''
     USERS.update({'david01': {'name': 'David'}})
 
 @step(u'When I retrieve the customer \'(.*)\'')
@@ -28,9 +28,11 @@ def when_i_retrieve_the_customer_group1(step, username):
 
 @step(u'Then I should get a \'(.*)\' response')
 def then_i_should_get_a_group1_response_group2(step, expected_status_code):
+    '''Check the status_code.'''
     assert_equals(world.response.status_code, int(expected_status_code))
 
 @step(u'And the following user details are returned:')
 def and_the_following_user_details(step):
+    '''Check the correct user data is in the response.'''
     assert_equals(step.hashes, [json.loads(world.response.data)])
 
