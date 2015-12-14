35a96ddfe0e51f03761a0ec7e52bea09ef2743c3
David Ohlemacher <david.ohlemacher@verizon.com>
2015-12-14 01:34:53 -0500

    Add support for DELETE using BDD.

--- app/application.py
+++ app/application.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
-from flask import Flask, request, jsonify
+#curl -i -H "Accept: application/json" http://127.0.0.1:5000/users
+#curl -i -H "Accept: application/json" http://127.0.0.1:5000/users/zero01
+#curl -i -H "Accept: application/json" -X POST -d "firstName=james" http://localhost:5000/persons/person
+#curl -i -H "Accept: application/json" -X DELETE http://localhost:5000/users/users/zero01
+
+from flask import Flask, request, make_response, jsonify
 
 app = Flask(__name__)
 
@@ -11,27 +16,58 @@ USERS = {
 }
 
 GET = 'GET'
+DELETE = 'DELETE'
 
-@app.route("/user/<username>", methods=[GET])
+@app.route("/users/<username>", methods=[GET, DELETE])
 def access_users(username):
+    # GET
     if request.method == GET:
         user_details = USERS.get(username)
         if user_details:
             return jsonify(user_details)
         else:
-            return Response(status=404)
+            response = make_response(
+                'User %s not found' % username,
+                404)
+            return response
+    # DELETE
+    elif request.method == DELETE:
+        try:
+            #global USERS
+            user_details = USERS.get(username)
+            del USERS[username]
+            #return jsonify({'deleted': username})
+            response = make_response(
+                'User %s deleted' % username,
+                200)
+            return response
+        except KeyError:
+            #print 'KeyError'
+            response = make_response(
+                'User %s not found to delete' % username,
+                404)
+            return response
+        except Exception as e:
+            #print 'Unknown', e
+            response = make_response(
+                'Unknown error',
+                500
+            )
+            return response
+    # No support
     else:
-        print 'Error: methon %s not support' % \
-            request.method
+        return Response(status=404)
 
 @app.route("/users", methods=[GET])
 def users():
     return jsonify(USERS)
 
 @app.route("/")
-def welcom():
+def welcome():
     return "Welcome to my Behavior Driven Development REST server"
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
+
--- test/features/steps.py
+++ test/features/steps.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 '''
 Allow defining steps and store values to be used across each step in the
@@ -8,6 +9,7 @@ from nose.tools import assert_equals
 from app.application import app, USERS
 import json
 
+# Test user query
 @before.all
 def before_all():
     '''Setup for testing.'''
@@ -18,13 +20,13 @@ def given_some_users_are_in_the_system(step):
     '''Add a user to USERS for testing against.'''
     USERS.update({'david01': {'name': 'David'}})
 
-@step(u'When I retrieve the customer \'(.*)\'')
-def when_i_retrieve_the_customer_group1(step, username):
+@step(u'When I retrieve the user \'(.*)\'')
+def when_i_retrieve_the_user_group1(step, username):
     '''
     A capture group is used in the regular expression allowing us to
     pass in variables to the step. This allows for the reuse of steps.
     '''
-    world.response = world.app.get('/user/{}'.format(username))
+    world.response = world.app.get('/users/{}'.format(username))
 
 @step(u'Then I should get a \'(.*)\' response')
 def then_i_should_get_a_group1_response_group2(step, expected_status_code):
@@ -36,3 +38,21 @@ def and_the_following_user_details(step):
     '''Check the correct user data is in the response.'''
     assert_equals(step.hashes, [json.loads(world.response.data)])
 
+
+# Test existing user deletion
+@step(u'When I delete the user \'([^\']*)\'')
+def when_i_delete_the_user_group1(step, username):
+    world.response = world.app.delete('/users/{}'.format(username))
+
+@step(u'And the message \'([^\']*)\' is returned')
+def and_the_message_group1_is_returned(step, group1):
+    assert_equals(world.response.data, group1)
+
+@step(u'And the user \'([^\']*)\' is removed')
+def and_the_user_group1_is_removed(step, group1):
+    world.response = world.app.get('/users/{}'.format(group1))
+    assert_equals(world.response.status_code, int(404))
+
+@step(u'And the following message is returned:')
+def and_the_following_message_is_returned(step):
+    assert False, 'This step must be implemented'
